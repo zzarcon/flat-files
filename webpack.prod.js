ba77f50a6dda109255710ea19a98b1bedbe2b415
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-10-29 21:19:23 +1100

    Use different webpack config for prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,11 +1,15 @@
 const path = require('path');
 const { CheckerPlugin } = require('awesome-typescript-loader');
 
+console.log('using production config...');
+
 module.exports = {
-  entry: './example/index.ts',
+  entry: './src/index.ts',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'index.js'
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'flatFiles',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx']
