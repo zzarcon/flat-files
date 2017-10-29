6e76b3b81a5724a52207df4a942f833168f7b7bb
Hector Leon Zarco Garcia <hezarco@gmail.com>
2017-10-29 21:39:23 +1100

    Use last version of flat-files

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import flatFiles from 'flat-files';
 
-console.log(flatFiles)
+const dropzone = document.querySelector('#dropzone');
 
-flatFiles(document.querySelector('#dropzone'), (files) => {
+flatFiles(dropzone, (files) => {
   files.forEach(file => {
     console.log(
       file.name,
       file.fullPath,
-      file.getMetadata()
+      file.getMetadata((metadata) => {
+        console.log(metadata);
+      })
     );
   })
 });
+
+const onDragFinish = (e) => {
+  const {target} = e;
+
+  target.classList.remove('drag-enter');
+  target.innerText = 'Drop something';
+};
+
+dropzone.addEventListener('dragenter', (e) => {
+  const {target} = e;
+
+  target.classList.add('drag-enter');
+  target.innerText = 'Release it!';
+});
+
+dropzone.addEventListener('dragleave', onDragFinish);
+dropzone.addEventListener('drop', onDragFinish);
